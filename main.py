4a6aa40081fa4499165ff293c3f401b19c126064
rohitmallem <mbvo17@student.bth.se>
2020-06-03 21:46:46 +0200

    added subtraction

--- main.py
+++ main.py
@@ -24,6 +24,23 @@ def addition():
         D = Fraction(input2)
         return str(float(C+D))#removed round function and results in float
 
+@app.route('/sub')
+def subtraction():
+    try:
+        input1=request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        input1='None'
+    try:
+        input2=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        input2='None'
+    if input1 == 'None' or input2 == 'None' :
+        return 'None'
+    else:
+        C = Fraction(input1)
+        D = Fraction(input2)
+        return str(float(C-D))
+
 
 if __name__ == "__main__":
     app.run()
