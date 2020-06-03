843d6db7a0a4ad05a57d19dfae0e271248739511
rohitmallem <mbvo17@student.bth.se>
2020-06-03 20:36:11 +0200

    added addition branch and test script

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from flask import Flask, request
-
+from fractions import Fraction
+from decimal import Decimal
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
-
+    return 'Usage;\nOperation?A=<input1>&B=<input2>\n'
 
 @app.route('/add')
 def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+    try:
+        input1=request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        input1='None'
+    try:
+        input2=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        input2='None'
+    if input1 == 'None' or input2 == 'None' :
+        return 'None'
+    else:
+        C = Fraction(input1)
+        D = Fraction(input2)
+        return str(float(C+D))#removed round function and results in float
+
 
 
 if __name__ == "__main__":
