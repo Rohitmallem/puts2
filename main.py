538b63f8dea3d0fd2d1352a538a6ac7bc394973f
Rohitmallem <43582088+Rohitmallem@users.noreply.github.com>
2020-06-04 04:34:05 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,82 @@
+from flask import Flask, request
+from fractions import Fraction
+from decimal import Decimal
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Usage;\nOperation?A=<value1>&B=<value2>\n'
+
+@app.route('/add')
+def addition():
+    try:
+        value1=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='Nothing'
+    try:
+        value2=request.args.get('F',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='Nothing'
+    if value1 == 'Nothing' or value2 == 'Nothing' :
+        return 'Nothing'
+    else:
+        A = Fraction(value1)
+        B = Fraction(value2)
+        return str(float(A+B))#removed round function and results in float
+
+@app.route('/sub')
+def subtraction():
+    try:
+        value1=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='Nothing'
+    try:
+        value2=request.args.get('F',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='Nothing'
+    if value1 == 'Nothing' or value2 == 'Nothing' :
+        return 'Nothing'
+    else:
+        A = Fraction(value1)
+        B = Fraction(value2)
+        return str(float(A-B))
+
+
+@app.route('/mul')
+def multiplication():
+    try:
+        value1=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='Nothing'
+    try:
+        value2=request.args.get('F',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='Nothing'
+    if value1 == 'Nothing' or value2 == 'Nothing' :
+        return 'Nothing'
+    else:
+        A = Fraction(value1)
+        B = Fraction(value2)
+        return str(float(A*B))
+
+@app.route('/div')
+def division():
+    try:
+        value1=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='Nothing'
+    try:
+        value2=request.args.get('F',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='Nothing'
+    if value1 == 'Nothing' or value2 == 'Nothing' :
+        return 'Nothing'
+    else:
+        A = Fraction(value1)
+        B = Fraction(value2)
+        try:
+            return str(float(A/B))
+        except ZeroDivisionError as error:
+            return 'Nothing'
+
+if __name__ == "__main__":
+    app.run()
