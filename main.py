3289241642e7d34adfa8b946305266c730ac3811
Rohitmallem <ravinrohit@gmail.com>
2020-06-04 04:21:31 +0200

    substraction

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def index():
 @app.route('/add')
 def addition():
     try:
-        value1=request.args.get('B',default = 0, type = Fraction)
+        value1=request.args.get('A',default = 0, type = Fraction)
     except ZeroDivisionError as error:
         value1='Nothing'
     try:
-        value2=request.args.get('F',default = 0, type = Fraction)
+        value2=request.args.get('B',default = 0, type = Fraction)
     except ZeroDivisionError as error:
         value2='Nothing'
     if value1 == 'Nothing' or value2 == 'Nothing' :
@@ -21,7 +21,7 @@ def addition():
     else:
         A = Fraction(value1)
         B = Fraction(value2)
-        return str(float(A+B))#removed round function and results in float
+        return str(float(A-B))#removed round function and results in float
 
 if __name__ == "__main__":
     app.run()
--- test.py
+++ test.py
@@ -7,35 +7,35 @@ class TestCalculator(unittest.TestCase):#unittest module provides a set of tools
 	def setUp(self):#setUp , when unittest module is used and it enaAles application to test
 		main.app.testing = True
 		self.app = main.app.test_client()
-
-	def test_add1(self):
+        
+    def test_sub1(self):
 		#case 1, A is n int B is an int type
-		sol = self.app.get('/add?=20&B=2')
-		self.assertEqual(b'22.0', sol.data)
+		sol = self.app.get('/sub?A=7&B=3')
+		self.assertEqual(b'4.0', sol.data)
 
-	def test_add2(self):
+	def test_sub2(self):
 
 		#case 2, A is rational no. and F is rational no. p/q form
-		sol = self.app.get('/add?A=5/3&B=7/2')
-		self.assertEqual(b'5.16666666667', sol.data)
+		sol = self.app.get('/sub?A=8/2&B=9/2')
+		self.assertEqual(b'-0.5', sol.data)
 
-	def test_add3(self):
+	def test_sub3(self):
 
-		#case 3, A is a float and F is F float
-		sol = self.app.get('/add?A=12.3&B=1.102')
-		self.assertEqual(b'13.402', sol.data)
+		#case 3, A is a float and B is a float
+		sol = self.app.get('/sub?A=70.2&B=1.901')
+		self.assertEqual(b'68.299', sol.data)
 
-	def test_add4(self):
+	def test_sub4(self):
 
-		#case 4, when A is float and B is int
-		sol = self.app.get('/add?A=32.32&B=67')
-		self.assertEqual(b'99.32', sol.data)
+		#case 4, when B is float and F is int
+		sol = self.app.get('/sub?A=32.32&B=4')
+		self.assertEqual(b'28.32', sol.data)
 
-	def test_add5(self):
+	def test_sub5(self):
 
 		#case 5, when A is int and B is float
-		sol = self.app.get('/add?A=110&B=68.7')
-		self.assertEqual(b'178.7', sol.data)
+		sol = self.app.get('/sub?A=22&B=7.201')
+		self.assertEqual(b'14.799', sol.data)
 
    
 if __name__ == '__main__':
